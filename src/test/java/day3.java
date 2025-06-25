f12f4f21ae6c8860959ced68710b68b3019cf621
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-06-25 14:22:28 +0700

    Addition Prioritizing the Test Case with TestNG

---
+++ src/test/java/day3.java
@@ -0,0 +1,24 @@
+
+//rahulonlinetutor@gmail.com
+
+import org.testng.annotations.Test;
+
+public class day3 {
+
+    @Test
+    public void WebLoginCarLoan() {
+        System.out.println("Web Login Car Loan");
+    }
+
+    @Test
+    public void MobileLoginCarLoan() {
+        //Appium
+        System.out.println("Mobile Login Car Loan");
+    }
+
+    @Test
+    public void LoginAPICarLoan3() {
+        //Rest API
+        System.out.println("API Login Car Loan");
+    }
+}
---
+++ src/test/java/day4.java
@@ -0,0 +1,24 @@
+
+//rahulonlinetutor@gmail.com
+
+import org.testng.annotations.Test;
+
+public class day4 {
+
+    @Test
+    public void WebLoginHomeLoan() {
+        System.out.println("Web Login Home Loan");
+    }
+
+    @Test
+    public void MobileLoginHomeLoan() {
+        //Appium
+        System.out.println("Mobile Login Home Loan");
+    }
+
+    @Test
+    public void LoginAPIHomeLoan3() {
+        //Rest API
+        System.out.println("API Login Home Loan");
+    }
+}
