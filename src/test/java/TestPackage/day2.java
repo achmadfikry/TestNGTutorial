cdab01fd1814fc32f72a5b8376eb4f9024403e2d
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-06-26 15:27:26 +0700

    Addition TestNG Annotation - BeforeTest & AfterTest

--- src/test/java/TestPackage/day2.java
+++ src/test/java/TestPackage/day2.java
@@ -1,10 +1,16 @@
 package TestPackage;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day2 {
     @Test
     public void ploan() {
         System.out.println("good");
+    }
+
+    @BeforeTest
+    public void prerequisite() {
+        System.out.println("I will execute first");
     }
 }
