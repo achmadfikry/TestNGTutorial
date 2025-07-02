c9126acdb7777e0ce406c2a6b82f970fab70cf96
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-07-02 10:31:25 +0700

    Addition TestNG Parameterization - test level / module level / shell level

--- src/test/java/TestPackage/day4.java
+++ src/test/java/TestPackage/day4.java
@@ -1,13 +1,16 @@
 package TestPackage;
 //rahulonlinetutor@gmail.com
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class day4 {
 
+    @Parameters({"URL"})
     @Test
-    public void WebLoginHomeLoan() {
-        System.out.println("Web Login Home Loan");
+    public void WebLoginHomeLoan(String uname) {
+        System.out.println("Web Login Home Personal Loan");
+        System.out.println(uname);
     }
 
     @Test(groups = {"Smoke"})
